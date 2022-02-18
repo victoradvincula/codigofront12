ff6e46689384d042bfcb1c7ee9d10853fad09576
victoradvincula <victoradvincula1@gmail.com>
2022-02-17 23:57:55 -0500

    170222

--- semana7/mvp-react/src/pages/contacto/contacto.jsx
+++ semana7/mvp-react/src/pages/contacto/contacto.jsx
@@ -1,8 +1,10 @@
 import "../contacto/contacto.scss";
-export const PageContacto = () =>{
-    return (
-        <div className="page-contacto">
-            MATEO 1
-        </div>
-    )
-}
+export const PageContacto = () => {
+  return (
+    <div className="page-contacto">
+      <div class="grid grid-cols-4 gap-4 text-3xl font-bold underline">
+        <input type="text" className="p-4 border-gray-500 rounded-xl focus:outline-none focus:border-gray-900"/>
+      </div>
+    </div>
+  );
+};
